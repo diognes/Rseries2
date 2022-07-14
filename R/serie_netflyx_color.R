a39e7c57d6483c9d0e562a806eae8b147e03080b
rodnm <rod_nm@outlook.com>
2022-07-13 19:05:17 -0500

    update

---
+++ R/serie_netflyx_color.R
@@ -0,0 +1,16 @@
+# serie netflyx palettes color
+
+palette_netflyx <- list(
+  Lupin        = c("#E26A40", "#0E263C", "#3B6183", "#5C85B1", 
+                   "#7BA0C6","#D9E2EC"),
+  Calamar      = c("#E7E1E4","#F5417C","#040404", "#343334", "#3C3C3C",
+                   "#585355","#747474","#7C7C7C"),
+  Castlevania   = c("#D3250D", "#721510", "#450B09", "#2F0605", 
+                    "#240504","#1C0404","#0A0404"),
+  Ozark         =c("#a9d2d0","#62a7af","#4d8a9d","#2b6b76","#1c5758","#123b4c"),
+  
+  GambitoDama   =c("#291f2a","#634039","#916b58","#be754c","#9a4d1b","#824529"),
+  
+  EstamosMuert = c("#c9cacc","#96aea8","#465e69","#5d5e4d","#1c4b46","#8f4031")
+  
+) 
